4a6ae34951a96f381db7d50702b6e59bc4dd6b59
TiagoDanin <TiagoDanin@outlook.com>
2019-01-08 19:36:49 -0300

    feat: create table and install ext in PostgreSQL

---
+++ scripts/postgresql.js
@@ -0,0 +1,29 @@
+const { Client } = require('pg')
+const client = new Client({
+	database: 'test'//'defendthecastle'
+})
+
+const log = (text) => console.log('>>', text)
+
+const main = async () => {
+	await client.connect()
+	await client.query(`
+		CREATE EXTENSION tsm_system_rows;
+		CREATE TABLE users(
+			id               INT     NOT NULL,
+			name             TEXT    NOT NULL,
+			type             TEXT    DEFAULT 'warrior',
+			level            INT     DEFAULT 1,
+			atack            INT     DEFAULT 50,
+			shield           INT     DEFAULT 50,
+			life             INT     DEFAULT 50,
+			money            INT     DEFAULT 100,
+			xp               INT     DEFAULT 0,
+			troops           INT     DEFAULT 5,
+			troops_time      DATE    DEFAULT now(),
+			PRIMARY KEY (id)
+		);
+	`, []).catch(log)
+	await client.end()
+}
+main()
