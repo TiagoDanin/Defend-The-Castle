bb83c187cf777a12be81621be3714f7008bcd3ac
TiagoDanin <TiagoDanin@outlook.com>
2019-02-17 19:44:59 -0300

    feat: upgrade users to v1.3.0

---
+++ scripts/update.1.3.0.js
@@ -0,0 +1,47 @@
+const fs = require('fs')
+
+const { Pool } = require('pg')
+const pool = new Pool({
+	database: 'test'//'defendthecastle'
+})
+
+const log = (text) => console.log('>>', text)
+
+const users = async () => {
+	let data = {}
+	const client = await pool.connect()
+	const query = 'select * from users;'
+	data = await client.query(query, [])
+	await client.release()
+	data.rows.forEach((db) => insert(db))
+	console.log('Done!')
+	return
+}
+
+const insert = async (db) => {
+	db.money += Math.floor(
+		(db.attack + db.shield + db.life) / 1.2
+	)
+	db.attack = 50
+	db.shield = 50
+	db.life = 50
+	const client = await pool.connect()
+	const query = `
+		UPDATE users SET
+			money = $1,
+			attack = $2,
+			shield = $3,
+			life = $4
+		WHERE id = $5;
+	`
+	await client.query(query, [
+		db.money,
+		db.attack,
+		db.shield,
+		db.life,
+		db.id
+	]).catch(log)
+	return await client.release()
+}
+
+users()
