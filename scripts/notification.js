209059b4ebbd8c7659a1267b63f1b3fe182a68ec
TiagoDanin <TiagoDanin@outlook.com>
2019-02-17 20:49:52 -0300

    feat: send global notification

---
+++ scripts/notification.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const Telegraf = require('telegraf')
+const { Pool } = require('pg')
+const pool = new Pool({
+	database: 'test'//'defendthecastle'
+})
+
+const bot = new Telegraf(process.env.telegram_token, {
+	username: 'DefendTheCastleBot'
+})
+
+const log = (text) => console.log('>>', text)
+
+const main = async () => {
+	let data = {}
+	const client = await pool.connect()
+	const query = 'select id, name, notification from users where notification;'
+	data = await client.query(query, [])
+	await client.release()
+	data.rows.forEach((db) => {
+		bot.telegram.sendMessage(db.id,
+			`
+*Global Notification!*
+• Version 1.1.x
+			`, {
+				parse_mode: 'Markdown'
+			}
+		)
+	})
+	console.log('Done!')
+	return
+}
+main()
