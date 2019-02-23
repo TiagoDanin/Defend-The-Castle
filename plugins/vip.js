84d8e8274c6f83977cbd4ebd2939643602268603
TiagoDanin <TiagoDanin@outlook.com>
2019-02-23 20:13:43 -0300

    feat: store vip

--- plugins/vip.js
+++ plugins/vip.js
@@ -1,89 +1,62 @@
 const base = async (ctx) => {
 	let text = `
 <b>${ctx.db.castle} City:</b> ${ctx.db.name}
-<b>💰 Money:</b> ${ctx.db.money} Coin (${ctx.db.moneyPerHour}/hour)
-${ctx.tips(ctx)}
----------------------------------------
-⚔️ ${ctx.db.attack}
-🛡 ${ctx.db.shield}
-❤️ ${ctx.db.life}
-`
+<b>💎 Diamonds:</b> ${ctx.db.diamond}
+${ctx.tips(ctx)}`
 
-	let items = {
-		'1': {
-			icon: '⚔️',
-			name: 'Attack',
-			value: 'attack',
-			upgrade: 0.09
-		},
-		'2': {
-			icon: '🛡',
-			name: 'Shield',
-			value: 'shield',
-			upgrade: 0.07
-		},
-		'3': {
-			icon: '❤️',
-			name: 'Life',
-			value: 'life',
-			upgrade: 0.057
-		}
-	}
-	Object.keys(items).map((e) => {
-		items[e].price = Math.floor(
-			Math.pow(
-				100,
-				Math.pow(ctx.db[items[e].value]+10, items[e].upgrade)
-			)
-		)
+	const itemsIds = Object.keys(ctx.items).filter((id) => {
+		if (ctx.items[id].qt) return true
+		return false
+	})
+	let items = {}
+	itemsIds.forEach((id) => {
+		items[id] = ctx.items[id]
 	})
 
-	if (ctx.match[2] && ctx.match[3] == 'up') {
-		let item = items[ctx.match[2].toString()]
-		if (item && ctx.db.money >= item.price) {
-			ctx.db.money -= item.price
-			ctx.db.money = Math.floor(ctx.db.money)
-			ctx.db[item.value] = Math.floor(ctx.db[item.value] + 10)
-			text = `
+	if (ctx.match[2]) {
+		let item = {...items[ctx.match[2]]}
+		if (ctx.match[3] == 'up') {
+			if (ctx.db.diamond - item.price >= 0) {
+				ctx.db.diamond = ctx.db.diamond - item.price
+				ctx.db.inventory = ctx.db.inventory.reduce((total, id, index) => {
+					if (id == 11 && item.price > 0) {
+						item.price--
+					} else {
+						total.push(id)
+					}
+					return total
+				}, [])
+				ctx.db.inventory.push(ctx.match[2])
+				await ctx.database.updateUser(ctx.from.id, 'inventory', ctx.db.inventory)
+				ctx.answerCbQuery('Inventory update!')
+			} else {
+				ctx.answerCbQuery(`💎 Your diamonds: ${ctx.db.diamond} | Price: ${item.price}`, true)
+				text += '\nFailed!'
+			}
+		}
+		text = `
 <b>${ctx.db.castle} City:</b> ${ctx.db.name}
-<b>💰 Money:</b> ${ctx.db.money} Coin (${ctx.db.moneyPerHour}/hour)
+<b>💎 Diamonds:</b> ${ctx.db.diamond}
 ${ctx.tips(ctx)}
 ---------------------------------------
-⚔️ ${ctx.db.attack}
-🛡 ${ctx.db.shield}
-❤️ ${ctx.db.life}
-
-Upgraded!`
-			ctx.database.updateUser(ctx.from.id, item.value, ctx.db[item.value]).then((res) => {
-				if (res) {
-					ctx.database.updateUser(ctx.from.id, 'money', ctx.db.money)
-				}
-			})
-			Object.keys(items).map((e) => {
-				items[e].price = Math.floor(
-					Math.pow(
-						100,
-						Math.pow(ctx.db[items[e].value]+10, items[e].upgrade)
-					)
-				)
-			})
-			ctx.answerCbQuery('Upgraded!')
-		} else {
-			ctx.answerCbQuery(`❌ Your money ${ctx.db.money} | Price ${item.price}`, true)
-			text += '\nFailed!'
-		}
+<b>${item.icon} ${item.name}</b>
+${item.desc}
+<b>💎 Price:</b> ${items[ctx.match[2]].price}
+			`
 	}
 
-	const mainKeyboard = Object.keys(items).reduce((total, id, index) => {
+	const mainKeyboard = itemsIds.reduce((total, id, index) => {
+		let qt = ctx.db.inventory.filter(i => i == id).length || 0
 		total.push([{
-			text: `${items[id.toString()].icon} ${items[id.toString()].name}`,
-			callback_data: `base:${id}`
+			text: `${items[id.toString()].icon} ${items[id.toString()].name} (${qt})`,
+			callback_data: `vip:${id}`
 		}, {
-			text: `💶 ${items[id.toString()].price}`,
-			callback_data: `base:${id}:up`
+			text: `💎 ${items[id.toString()].price}`,
+			callback_data: `vip:${id}:up`
 		}])
 		return total
 	}, [])
+
 	const keyboard = [
 		...mainKeyboard,
 		[{
@@ -102,7 +75,7 @@ Upgraded!`
 }
 
 module.exports = {
-	id: 'base',
+	id: 'vip',
 	callback: base,
 	onlyUser: true
 }
