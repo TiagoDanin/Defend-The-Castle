c4de6a84e2c4f96820f4f85e2cf69961c37363b2
TiagoDanin <TiagoDanin@outlook.com>
2019-02-27 10:54:12 -0300

    fix: keyboard in Telegram X

--- plugins/stats.js
+++ plugins/stats.js
@@ -2,9 +2,9 @@ const base = async (ctx) => {
 	const join24 = await ctx.database.getJoin24()
 	const all = await ctx.database.getAllUsers()
 	const text = `
-<b>New Players (24h):</b> ${join24.length}
 <b>Total Players:</b> ${all.length}
-<b>Online Players:</b> ${(all.filter((e) => e.online)).length}`
+<b>New Players (24h):</b> ${join24.length}
+<b>Online Players (24h):</b> ${(all.filter((e) => e.online)).length}`
 
 	return ctx.editMessageText(text + ctx.fixKeyboard, {
 		parse_mode: 'HTML',
