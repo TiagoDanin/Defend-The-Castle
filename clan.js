883e2f094ca3126638d6890131df9f471a759dfa
TiagoDanin <TiagoDanin@outlook.com>
2019-03-05 15:11:08 -0300

    feat: clan v1 done

--- clan.js
+++ clan.js
@@ -1,27 +1,27 @@
 module.exports = {
 	1: {
-		xp: 100,
+		xp: 100 * 3,
 		members: 3,
 		money: 100
 	},
 	2: {
-		xp: 1000,
+		xp: 1000 * 3,
 		members: 3,
 		money: 1000
 	},
 	3: {
-		xp: 10000,
+		xp: 10000 * 4,
 		members: 4,
-		money: 1500
+		money: 5000
 	},
 	4: {
-		xp: 20000,
+		xp: 25000 * 5,
 		members: 5,
-		money: 3000
+		money: 5500
 	},
 	5: {
-		xp: 50000,
+		xp: 50000 * 5,
 		members: 6,
-		money: 5000
+		money: 7000
 	}
 }
--- plugins/clan.js
+++ plugins/clan.js
@@ -1,10 +1,27 @@
 const processClan = async (ctx, clan) => {
 	const moneyPerSecond = (ctx.clan[clan.level].money / 60) / 60
+	let xpNextLevel = 99999999999999999999
+	clan.levelPoc = 1
+	const levels = Object.keys(ctx.clan)
+	if (clan.level < levels.length && ctx.clan[clan.level+1]) {
+		xpNextLevel = ctx.clan[clan.level+1].xp
+		clan.levelPoc = Math.floor(
+			clan.xp / (
+				xpNextLevel / 100
+			)
+		)
+	}
 	if (clan.timerunning >= 120) {
 		clan.money += (clan.timerunning * moneyPerSecond)
+		if (clan.xp >= xpNextLevel) {
+			clan.level++
+			clan.xp -= xpNextLevel
+		}
 		await ctx.database.updateClan({
 			id: clan.id,
-			money: clan.money
+			money: Math.floor(clan.money),
+			level: Math.floor(clan.level),
+			xp: Math.floor(clan.xp)
 		})
 	}
 	return clan
@@ -13,7 +30,7 @@ const processClan = async (ctx, clan) => {
 const processView = (ctx, clan) => {
 	return `
 <b>🌇 Name:</b> ${clan.name} [${clan.flag}]
-<b>🏅 Level:</b> ${clan.level}
+<b>🏅 Level:</b> ${clan.level}${clan.levelPoc ? ` (${clan.levelPoc}%)` : ''}
 <b>🎖 Experience:</b> ${ctx.nl(clan.xp)}
 <b>💰 Money:</b> ${ctx.nl(clan.money)} (${ctx.nl(ctx.clan[clan.level].money)}/hour)
 <b>👥 Members:</b> ${clan.members.length}/${ctx.clan[clan.level].members}`
@@ -74,7 +91,24 @@ const base = async (ctx) => {
 		text = processView(ctx, clan)
 	}
 
-	if (ctx.match[2] == 'members') {
+	if (ctx.match[2] == 'money') {
+		text = `💰 Transferred to your account: ${ctx.nl(clan.money)}`
+		ctx.db.money += Math.floor(clan.money)
+		await ctx.database.updateClan({
+			id: clan.id,
+			money: 0
+		})
+		await ctx.database.saveUser(ctx)
+	} else if (ctx.match[2] == 'xp') {
+		text = `✨ Transferred to clan: ${ctx.nl(ctx.db.xp)}`
+		clan.xp += ctx.db.xp
+		ctx.db.xp = 0
+		await ctx.database.updateClan({
+			id: clan.id,
+			xp: Math.floor(clan.xp)
+		})
+		await ctx.database.saveUser(ctx)
+	} else if (ctx.match[2] == 'members') {
 		text = '<b>Members (wins;losses):\n</b>'
 		for (let i = 0; i < clan.members.length; i++) {
 			let member = clan.members[i]
