4399da7a94a6ad274bb081ac0aedccad283e5fdb
TiagoDanin <TiagoDanin@outlook.com>
2019-03-06 14:05:04 -0300

    feat: command /add to admin

--- plugins/thanks.js
+++ plugins/thanks.js
@@ -12,15 +12,26 @@ const base = async (ctx) => {
 			db: play
 		}
 		user.db.old = {...user.db}
-		user.db.xp += 1200
-		user.db.money += 5000
-		user.db.inventory.push('11')
-		ctx.replyWithHTML(`
+		if (ctx.match[1] == 'thanks') {
+			user.db.xp += 1200
+			user.db.money += 5000
+			user.db.inventory.push('11')
+			ctx.replyWithHTML(`
 <b>Thanks!</b>
 + 1200 XP
 + 5000 Money
 + 1 Diamond
-		`)
+			`)
+		} else {
+			for (let i = 0; i < Number(ctx.match[4]); i++) {
+				if (ctx.match[2] == 'inventory') {
+					user.db.inventory.push(Number(ctx.match[3]))
+				} else {
+					user.db[ctx.match[2]] += Number(ctx.match[3])
+				}
+			}
+			ctx.replyWithHTML(`<b>ADD:</b> ${ctx.match[2]} += ${ctx.match[3]} * ${ctx.match[4]}`)
+		}
 		await ctx.database.saveUser(user)
 		return
 	}
@@ -31,6 +42,7 @@ module.exports = {
 	id: 'add',
 	plugin: base,
 	regex: [
-		/^\/thanks/i
+		/^\/(thanks)/i,
+		/^\/(add) (\w*) (\d*) (\d*)/i
 	]
 }
