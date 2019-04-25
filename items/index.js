77b686eb97f97145a8c0b99bf9f0cdb40707e13a
TiagoDanin <TiagoDanin@outlook.com>
2019-04-25 15:00:45 -0300

    feat: index items

---
+++ items/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+	...require('./bank'), //5
+	...require('./bomb'), //1
+	...require('./clone'), //10
+	...require('./diamond'), //11
+	...require('./hospital'), //7
+	...require('./null'), //0
+	...require('./rocket'), //4
+	...require('./shield'), //12
+	...require('./syringe'), //13
+	...require('./tower'), //6
+	...require('./zones') //2, 3
+}
