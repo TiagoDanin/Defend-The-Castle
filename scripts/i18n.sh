2b3bbff3d7f9a4540210409526f0cb24ec9b793e
TiagoDanin <TiagoDanin@outlook.com>
2019-11-17 13:44:39 -0300

    fix: i18n script

--- scripts/i18n.sh
+++ scripts/i18n.sh
@@ -1,2 +1,2 @@
-node scripts/getTextItems.js
+node scripts/get-text-items.js
 (echo itemsAllText.js && echo base/tips.js && echo base/quest.js && find items/ && find plugins/) | grep .js | sort | npx nodejs-i18n --value | msgmerge --backup=off --update locales/pt.po /dev/stdin
